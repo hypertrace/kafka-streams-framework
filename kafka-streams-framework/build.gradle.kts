c28a404994783ebe3ab701e1df918af3f543fa71
Laxman Ch <laxman@traceable.ai>
2020-12-20 20:25:25 +0530

    Snyk build fix

--- kafka-streams-framework/build.gradle.kts
+++ kafka-streams-framework/build.gradle.kts
@@ -11,32 +11,14 @@ tasks.test {
 
 dependencies {
     api(project(":kafka-streams-serdes"))
-    api("org.apache.kafka:kafka-streams:5.5.1-ccs")
+    api("org.apache.kafka:kafka-streams:6.0.1-ccs")
     api("com.typesafe:config:1.4.1")
-    api("io.confluent:kafka-streams-avro-serde:5.5.0")
+    api("io.confluent:kafka-streams-avro-serde:6.0.1")
     implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.18")
     implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.18")
-    implementation("org.apache.kafka:kafka-clients:5.5.1-ccs")
-    testImplementation("org.apache.kafka:kafka-streams-test-utils:5.5.1-ccs")
+    implementation("org.apache.kafka:kafka-clients:6.0.1-ccs")
+    testImplementation("org.apache.kafka:kafka-streams-test-utils:6.0.1-ccs")
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation("org.mockito:mockito-core:3.6.28")
     testImplementation("org.hamcrest:hamcrest-core:2.2")
-    constraints {
-        implementation("com.google.guava:guava:30.0-jre") {
-            because("Deserialization of Untrusted Data [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-32236] in com.google.guava:guava@20.0\n" +
-                    "   introduced by io.confluent:kafka-avro-serializer@5.5.0 > io.confluent:kafka-schema-registry-client@5.5.0 > io.swagger:swagger-core@1.5.3 > com.google.guava:guava@18.0")
-        }
-        implementation("org.hibernate.validator:hibernate-validator:6.1.6.Final") {
-            because("Cross-site Scripting (XSS) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGHIBERNATEVALIDATOR-541187] in org.hibernate.validator:hibernate-validator@6.0.17.Final\n" +
-                    "   introduced by io.confluent:kafka-avro-serializer@5.5.0 > io.confluent:kafka-schema-registry-client@5.5.0 > org.glassfish.jersey.ext:jersey-bean-validation@2.30 > org.hibernate.validator:hibernate-validator@6.0.17.Final")
-        }
-        implementation("org.yaml:snakeyaml:1.27") {
-            because("Denial of Service (DoS) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGYAML-537645] in org.yaml:snakeyaml@1.23\n" +
-                    "   introduced by io.confluent:kafka-avro-serializer@5.5.0 > io.confluent:kafka-schema-registry-client@5.5.0 > io.swagger:swagger-core@1.5.3 > com.fasterxml.jackson.dataformat:jackson-dataformat-yaml@2.4.5 > org.yaml:snakeyaml@1.12")
-        }
-        implementation("org.glassfish.jersey.media:jersey-media-jaxb:2.32"){
-            because("XML Entity Expansion [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGGLASSFISHJERSEYMEDIA-595972] in org.glassfish.jersey.media:jersey-media-jaxb@2.30\n" +
-                    "    introduced by io.confluent:kafka-avro-serializer@5.5.0 > io.confluent:kafka-schema-registry-client@5.5.0 > org.glassfish.jersey.ext:jersey-bean-validation@2.30 > org.glassfish.jersey.core:jersey-server@2.30 > org.glassfish.jersey.media:jersey-media-jaxb@2.30");
-        }
-    }
 }
