b8cdae7cec579f2ff102f2038d378311d3f1a286
ravisingal <62086374+ravisingal@users.noreply.github.com>
2021-04-09 12:02:16 +0530

    publish artifacts to hypertrace artifactory (#34)
    * publish artifacts to hypertrace artifactory
    
    * update service framework version

--- kafka-streams-serdes/build.gradle.kts
+++ kafka-streams-serdes/build.gradle.kts
@@ -12,7 +12,7 @@ tasks.test {
 
 dependencies {
     api("org.apache.kafka:kafka-streams:6.1.0-ccs")
-    implementation("org.apache.avro:avro:1.9.2")
+    implementation("org.apache.avro:avro:1.10.2")
     implementation("org.apache.kafka:kafka-clients:6.1.0-ccs")
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
     constraints {
