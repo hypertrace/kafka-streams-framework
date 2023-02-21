e668a6abb26cf585120e89d0a904b0a3ecc0e6f4
Laxman Ch <laxman@traceable.ai>
2023-02-21 19:32:39 +0530

    support key hash partitioner

--- kafka-streams-partitioners/weighted-group-partitioner/src/main/java/org/hypertrace/core/kafkastreams/framework/partitioner/KeyHashPartitioner.java
+++ kafka-streams-partitioners/weighted-group-partitioner/src/main/java/org/hypertrace/core/kafkastreams/framework/partitioner/KeyHashPartitioner.java
@@ -1,12 +1,9 @@
 package org.hypertrace.core.kafkastreams.framework.partitioner;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.kafka.common.utils.Utils;
 import org.apache.kafka.streams.processor.StreamPartitioner;
 
 public class KeyHashPartitioner<K, V> implements StreamPartitioner<K, V> {
-  private final AtomicInteger currentPartition = new AtomicInteger();
-
   @Override
   public Integer partition(String topic, K key, V value, int numPartitions) {
     int hashcode = 0;
