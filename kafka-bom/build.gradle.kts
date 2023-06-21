1a952a00e479e4674d63210c176bf2e7093a0a5b
Aaron Steinfeld <aaron@traceable.ai>
2023-06-21 10:00:40 -0400

    style: reformat

--- kafka-bom/build.gradle.kts
+++ kafka-bom/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    `java-platform`
-    id("org.hypertrace.publish-plugin")
+  `java-platform`
+  id("org.hypertrace.publish-plugin")
 }
 
 
 var kafkaVersion = "7.2.1"
-var kafkaCcsVersion ="$kafkaVersion-ccs"
+var kafkaCcsVersion = "$kafkaVersion-ccs"
 
 dependencies {
   constraints {
     api("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     api("org.xerial.snappy:snappy-java:1.1.10.1") {
-        because("[https://nvd.nist.gov/vuln/detail/CVE-2023-34455] in 'org.apache.kafka:kafka-clients:*'")
+      because("[https://nvd.nist.gov/vuln/detail/CVE-2023-34455] in 'org.apache.kafka:kafka-clients:*'")
     }
 
     api("io.confluent:kafka-streams-avro-serde:$kafkaVersion")
