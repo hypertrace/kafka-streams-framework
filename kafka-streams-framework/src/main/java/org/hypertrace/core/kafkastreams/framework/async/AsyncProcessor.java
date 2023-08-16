10140d42efd606b9dbd016d0abfae776446fce48
avinash <avinash@traceable.ai>
2023-08-16 15:33:45 +0530

    Implemented AsyncProcessor
    Inline with AsyncTransformer

--- kafka-streams-framework/src/main/java/org/hypertrace/core/kafkastreams/framework/async/AsyncProcessor.java
+++ kafka-streams-framework/src/main/java/org/hypertrace/core/kafkastreams/framework/async/AsyncProcessor.java
@@ -1,9 +1,85 @@
 package org.hypertrace.core.kafkastreams.framework.async;
 
+import com.google.common.util.concurrent.RateLimiter;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
 import org.apache.kafka.streams.processor.api.Record;
 
-public class AsyncProcessor implements Processor {
+@Slf4j
+public abstract class AsyncProcessor<K, V, KOUT, VOUT> implements Processor<K, V, KOUT, VOUT> {
+
+  private final Executor executor;
+  private final BlockingQueue<CompletableFuture<List<Record<KOUT, VOUT>>>> pendingFutures;
+  private final RateLimiter rateLimiter;
+  private ProcessorContext<KOUT, VOUT> context;
+
+  public AsyncProcessor(
+      Supplier<Executor> executorSupplier, AsyncTransformerConfig asyncTransformerConfig) {
+    this.executor = executorSupplier.get();
+    this.pendingFutures = new ArrayBlockingQueue<>(asyncTransformerConfig.getMaxBatchSize());
+    this.rateLimiter =
+        RateLimiter.create(1000.0 / asyncTransformerConfig.getCommitIntervalMs().toMillis());
+    log.info(
+        "async transformer config. maxBatchSize: {}, commit rate: {}",
+        this.pendingFutures.remainingCapacity(),
+        this.rateLimiter.getRate());
+    // warmup to prevent commit on first message
+    rateLimiter.tryAcquire();
+  }
+
   @Override
-  public void process(Record record) {}
+  public final void init(ProcessorContext<KOUT, VOUT> context) {
+    this.context = context;
+    doInit(context.appConfigs());
+  }
+
+  protected abstract void doInit(Map<String, Object> appConfigs);
+
+  public abstract List<Record<KOUT, VOUT>> asyncProcess(K key, V value);
+
+  @SneakyThrows
+  @Override
+  public void process(Record<K, V> record) {
+    CompletableFuture<List<Record<KOUT, VOUT>>> future =
+        CompletableFuture.supplyAsync(() -> asyncProcess(record.key(), record.value()), executor);
+    // with put, thread gets blocked when queue is full. queue consumer runs in this same thread.
+    pendingFutures.put(future);
+
+    // Flush based on size or time - whichever occurs first
+    // once the queue is full, flush the queue.
+    if (pendingFutures.remainingCapacity() == 0 || rateLimiter.tryAcquire()) {
+      log.debug("flush start - queue size before flush: {}", pendingFutures.size());
+      processResults();
+      log.debug("flush end - queue size after flush: {}", pendingFutures.size());
+    }
+  }
+
+  @SneakyThrows
+  private void processResults() {
+    while (!pendingFutures.isEmpty()) {
+      CompletableFuture<List<Record<KOUT, VOUT>>> future = pendingFutures.poll();
+      // makes sure transformation is complete
+      future.join();
+      // another join is needed to make sure downstream forward is also complete
+      future
+          .thenAccept(
+              result -> {
+                if (result != null) {
+                  result.forEach(record -> context.forward(record));
+                }
+              })
+          .join();
+    }
+    // commit once per batch
+    context.commit();
+  }
 }
