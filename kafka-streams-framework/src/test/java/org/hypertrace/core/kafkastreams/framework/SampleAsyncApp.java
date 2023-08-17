391c4f2c53cf52d0bb4ff9057c565eacd8c43b1c
avinash <avinash@traceable.ai>
2023-08-17 15:26:05 +0530

    Removed AsyncTransformer completely

--- kafka-streams-framework/src/test/java/org/hypertrace/core/kafkastreams/framework/SampleAsyncApp.java
+++ kafka-streams-framework/src/test/java/org/hypertrace/core/kafkastreams/framework/SampleAsyncApp.java
@@ -7,15 +7,12 @@ import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.Record;
 import org.hypertrace.core.kafkastreams.framework.async.AsyncProcessor;
 import org.hypertrace.core.kafkastreams.framework.async.AsyncProcessorConfig;
-import org.hypertrace.core.kafkastreams.framework.async.AsyncTransformer;
-import org.hypertrace.core.kafkastreams.framework.async.AsyncTransformerConfig;
 import org.hypertrace.core.kafkastreams.framework.async.ChildRecord;
 import org.hypertrace.core.kafkastreams.framework.async.ExecutorFactory;
 import org.hypertrace.core.kafkastreams.framework.constants.KafkaStreamsAppConstants;
@@ -89,31 +86,6 @@ class SlowProcessor extends AsyncProcessor<String, String, String, String> {
     return List.of(
         new ChildRecord<>(
             null, new Record<>("out:" + key, "out:" + value, System.currentTimeMillis())));
-  }
-}
-
-@Slf4j
-class SlowTransformer extends AsyncTransformer<String, String, String, String> {
-
-  public SlowTransformer(
-      Supplier<Executor> executorSupplier, AsyncTransformerConfig asyncTransformerConfig) {
-    super(executorSupplier, asyncTransformerConfig);
-  }
-
-  @Override
-  protected void doInit(Map<String, Object> appConfigs) {
-    // no-op
-  }
-
-  @SneakyThrows
-  @Override
-  public List<KeyValue<String, String>> asyncTransform(String key, String value) {
-    if (!key.startsWith("key")) {
-      return null;
-    }
-    log.info("transforming - key: {}, value: {}", key, value);
-    Thread.sleep(25);
-    return List.of(KeyValue.pair("out:" + key, "out:" + value));
   }
 }
 
