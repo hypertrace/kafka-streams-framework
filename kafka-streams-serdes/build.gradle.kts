7cf6d31917487df070d9030f086a38b6fc8ec4b9
singh-viikram <vikram.singh@traceable.ai>
2024-09-13 12:25:32 +0530

    nits

--- kafka-streams-serdes/build.gradle.kts
+++ kafka-streams-serdes/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
   api("org.apache.kafka:kafka-clients")
   api("org.apache.avro:avro")
-  api("com.google.protobuf:protobuf-java-util")
+  api("com.google.protobuf:protobuf-java-util:3.21.7")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
