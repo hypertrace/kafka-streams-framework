31576cf7549e45f2a6a808013efc90b72df70dfc
singh-viikram <vikram.singh@traceable.ai>
2024-09-13 12:11:32 +0530

    nits

--- kafka-streams-serdes/build.gradle.kts
+++ kafka-streams-serdes/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   `java-library`
   jacoco
-  id("com.google.protobuf") version "0.9.3"
   id("org.hypertrace.avro-plugin")
   id("org.hypertrace.publish-plugin")
   id("org.hypertrace.jacoco-report-plugin")
@@ -24,12 +23,4 @@ dependencies {
 // Disabling compatibility check for the test avro definitions.
 tasks.named<org.hypertrace.gradle.avro.CheckAvroCompatibility>("avroCompatibilityCheck") {
   enabled = false
-}
-
-sourceSets {
-  test {
-    java {
-      srcDirs("src/test/proto") // Generated Proto classes
-    }
-  }
 }
--- kafka-streams-serdes/src/test/java/org/hypertrace/core/kafkastreams/framework/serdes/ProtoSerdeTest.java
+++ kafka-streams-serdes/src/test/java/org/hypertrace/core/kafkastreams/framework/serdes/ProtoSerdeTest.java
@@ -1,38 +1,38 @@
 package org.hypertrace.core.kafkastreams.framework.serdes;
 
+import com.google.protobuf.Value;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serializer;
 import org.hypertrace.core.kafkastreams.framework.serdes.proto.ProtoDeserializer;
 import org.hypertrace.core.kafkastreams.framework.serdes.proto.ProtoSerializer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import proto.TestProtoRecordOuterClass.TestProtoRecord;
 
 public class ProtoSerdeTest {
 
   private static final String TEST_TOPIC = "test-topic";
 
   // Subclass for testing with proto deserialization
-  public static class TestProtoRecordDeserializer extends ProtoDeserializer<TestProtoRecord> {
+  public static class TestProtoRecordDeserializer extends ProtoDeserializer<Value> {
     public TestProtoRecordDeserializer() {
-      super(TestProtoRecord.parser());
+      super(Value.parser());
     }
   }
 
   @Test
   public void testSerialize() {
-    Serializer<TestProtoRecord> serializer = new ProtoSerializer<>();
+    Serializer<Value> serializer = new ProtoSerializer<>();
 
-    Deserializer<TestProtoRecord> deserializer = new TestProtoRecordDeserializer();
-    TestProtoRecord message = TestProtoRecord.newBuilder().setId("id").build();
+    Deserializer<Value> deserializer = new TestProtoRecordDeserializer();
+    Value message = Value.newBuilder().setStringValue("id").build();
 
     byte[] serializedData = serializer.serialize(TEST_TOPIC, message);
 
     Assertions.assertNotNull(serializedData);
     Assertions.assertTrue(serializedData.length > 0);
 
-    TestProtoRecord deserializedMessage = deserializer.deserialize(TEST_TOPIC, serializedData);
+    Value deserializedMessage = deserializer.deserialize(TEST_TOPIC, serializedData);
 
-    Assertions.assertEquals(message.getId(), deserializedMessage.getId());
+    Assertions.assertEquals(message.getStringValue(), deserializedMessage.getStringValue());
   }
 }
