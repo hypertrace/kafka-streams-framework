44f088420343c2f4f428dc09cb424a3c6501d941
Aaron Steinfeld <aaron@traceable.ai>
2024-10-04 11:21:55 -0400

    chore: update some old dependencies

--- kafka-bom/build.gradle.kts
+++ kafka-bom/build.gradle.kts
@@ -6,18 +6,16 @@ plugins {
 
 var confluentVersion = "7.7.0"
 var confluentCcsVersion = "$confluentVersion-ccs"
-var protobufVersion = "3.21.7"
+var protobufVersion = "3.25.5"
 
 dependencies {
   constraints {
-    api("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    api("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     api("org.xerial.snappy:snappy-java:1.1.10.5") {
       because("[https://nvd.nist.gov/vuln/detail/CVE-2023-34455] in 'org.apache.kafka:kafka-clients:*'")
       because("[https://nvd.nist.gov/vuln/detail/CVE-2023-43642]")
     }
-    api("com.google.protobuf:protobuf-java-util:3.21.7") {
-      because("https://nvd.nist.gov/vuln/detail/CVE-2022-3171")
-    }
+    api("com.google.protobuf:protobuf-java-util:$protobufVersion")
     api("com.squareup.okio:okio:3.4.0") {
       because("https://nvd.nist.gov/vuln/detail/CVE-2023-3635 in io.confluent:kafka-protobuf-serializer:7.4.0")
     }
@@ -32,6 +30,6 @@ dependencies {
     api("org.apache.kafka:kafka-clients:$confluentCcsVersion")
     api("org.apache.kafka:kafka-streams:$confluentCcsVersion")
     api("org.apache.kafka:kafka-streams-test-utils:$confluentCcsVersion")
-    api("org.apache.avro:avro:1.11.3")
+    api("org.apache.avro:avro:1.11.4")
   }
 }
