85ad9b7796ecffb81faa80ffc0b46532c9a76b0f
Aaron Steinfeld <aaron@traceable.ai>
2025-06-26 17:44:48 -0400

    build: update repos and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import org.hypertrace.gradle.publishing.HypertracePublishExtension
 import org.hypertrace.gradle.publishing.License
 
 plugins {
-  id("org.hypertrace.repository-plugin") version "0.4.2"
-  id("org.hypertrace.ci-utils-plugin") version "0.3.2"
-  id("org.hypertrace.avro-plugin") version "0.4.0" apply false
-  id("org.hypertrace.publish-plugin") version "1.0.5" apply false
-  id("org.hypertrace.jacoco-report-plugin") version "0.2.1" apply false
-  id("org.hypertrace.code-style-plugin") version "2.0.0" apply false
-  id("org.owasp.dependencycheck") version "8.2.1"
+  id("org.hypertrace.repository-plugin") version "0.5.0"
+  id("org.hypertrace.ci-utils-plugin") version "0.4.0"
+  id("org.hypertrace.avro-plugin") version "0.5.0" apply false
+  id("org.hypertrace.publish-plugin") version "1.1.1" apply false
+  id("org.hypertrace.jacoco-report-plugin") version "0.3.0" apply false
+  id("org.hypertrace.code-style-plugin") version "2.1.2" apply false
+  id("org.owasp.dependencycheck") version "12.1.0"
 }
 
 subprojects {
