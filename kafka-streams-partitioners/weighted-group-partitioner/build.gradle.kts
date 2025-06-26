758888d57fc0d18cd01ab55dea4d3f202044a707
Aaron Steinfeld <aaron@traceable.ai>
2025-06-26 19:08:43 -0400

    chore: update deps

--- kafka-streams-partitioners/weighted-group-partitioner/build.gradle.kts
+++ kafka-streams-partitioners/weighted-group-partitioner/build.gradle.kts
@@ -15,11 +15,11 @@ dependencies {
 
   api(platform(project(":kafka-bom")))
   api("org.apache.kafka:kafka-streams")
-  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.7")
+  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.15")
   api("com.typesafe:config:1.4.2")
   implementation("com.google.guava:guava:32.0.1-jre")
-  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.13.7")
-  implementation("org.hypertrace.config.service:partitioner-config-service-api:0.1.46")
+  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.13.15")
+  implementation("org.hypertrace.config.service:partitioner-config-service-api:0.1.73")
   implementation("org.slf4j:slf4j-api:1.7.36")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
